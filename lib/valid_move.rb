9552ca2902d4a9e53f2a9f0963bcf5a7f18c4903
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-09-29 15:27:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,14 +5,20 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+=begin
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+=end
 
-# Checks that the move entered is a valid move, by checking that the position is not taken and that the number of
+def position_taken? (board, index)
+  board[index] != " " ||  board[index] != "" || board[index] != nil
+end
+=begin
 def valid_move?(board, index)
   if position_taken?(board,index) || index < 0 || index > 8
     return false
   elsif !position_taken?(board, index)
     return true
   end
+=end
